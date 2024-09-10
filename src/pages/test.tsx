f6983013f59f715c1df6c17451e03eb73206d61c
ameer_hamza0331 <hafizameerhamza87@gmail.com>
2024-09-10 09:25:07 +0500

    try to fix session

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,3 +1,5 @@
+import { getServerSession } from 'next-auth';
+import { getSession } from 'next-auth/react';
 import React from 'react'
 
 export default function Test() {
@@ -5,3 +7,20 @@ export default function Test() {
     <div>Test</div>
   )
 }
+export async function getServerSideProps(context:any) {
+    const session = await getSession(context);
+    console.log("session",session)
+  
+    if (!session) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        },
+      };
+    }
+  
+    return {
+      props: { session },
+    };
+  }
